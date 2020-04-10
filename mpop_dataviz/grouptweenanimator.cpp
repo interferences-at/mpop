1882ba8f86bfb13225c56c9a914a54356af91dc2
Alexandre Quessy <alexandre@quessy.net>
2020-04-10 15:39:38 -0400

    Seem to be done with grouptweenanimator

--- mpop_dataviz/grouptweenanimator.cpp
+++ mpop_dataviz/grouptweenanimator.cpp
@@ -1,8 +1,14 @@
 #include "grouptweenanimator.h"
+#include <QDebug>
+
+static const qreal ZERO_VAL = 0.0;
+static const qreal ONE_VAL = 0.0;
 
 GroupTweenAnimator::GroupTweenAnimator()
 {
-
+    this->_easingType = QEasingCurve::Linear; // default
+    this->_startTime = 0; // default
+    this->_endTime = 10000000L; // FIXME default
 }
 
 
@@ -12,25 +18,89 @@ GroupTweenAnimator::~GroupTweenAnimator()
 }
 
 
-void GroupTweenAnimator::addSceneObjects(const QVector<SceneObject*>& sceneObjects) {
-    _sceneObjects.append(sceneObjects);
+//void GroupTweenAnimator::addSceneObjects(const QVector<SceneObject*>& sceneObjects) {
+//
+//    //AnimatedSceneObject* animatedSceneObject
+//    _sceneObjects.append(sceneObjects);
+//}
+
+void GroupTweenAnimator::addSceneObjectToAnimate(SceneObject* sceneObject, qreal targetX, qreal targetY, qreal targetRotation) {
+    AnimatedSceneObjectPtr animatedObj; // calls new AnimatedSceneObject (?)
+    animatedObj->sceneObject = sceneObject;
+
+    animatedObj->xFrom = sceneObject->getX();
+    animatedObj->xTo = targetX;
+
+    animatedObj->yFrom = sceneObject->getY();
+    animatedObj->yTo = targetY;
+
+    animatedObj->rotationFrom = sceneObject->getRotation();
+    animatedObj->rotationTo = targetRotation;
+
+    this->_animatedSceneObjects.append(animatedObj);
 }
 
-
+// static method
 qreal GroupTweenAnimator::mapValue(qreal inValue, qreal inFrom, qreal inTo, qreal outFrom, qreal outTo) {
+    if ((inTo - inFrom) <= 0.0) {
+        qDebug() << "mapValue: division by zero!";
+        return 0.0;
+    }
     qreal ret = outFrom + (outTo - outFrom) * ((inValue - inFrom) / (inTo - inFrom));
     // FIXME check for a division by zero
     return ret;
 }
 
 
-bool GroupTweenAnimator::updateSceneObjects(const qint64& currentTime) {
+bool GroupTweenAnimator::updateSceneObjectsPosition(const qint64& currentTime) {
     bool hasSomeTimeLeft = ! this->isDone(currentTime);
-    if (hasSomeTimeLeft) {
 
+    for (auto iter = this->_animatedSceneObjects.begin(); iter != _animatedSceneObjects.end(); ++ iter) {
+        AnimatedSceneObjectPtr animatedData = (*iter);
+        SceneObject* sceneObject = animatedData->sceneObject;
+
+        if (hasSomeTimeLeft) {
+            qreal elapsedRatio = this->getElapsedRatio(currentTime);
+
+            qreal newX = GroupTweenAnimator::mapValue(elapsedRatio, ZERO_VAL, ONE_VAL,
+                animatedData->xFrom, animatedData->xTo);
+            qreal newY = GroupTweenAnimator::mapValue(elapsedRatio, ZERO_VAL, ONE_VAL,
+                animatedData->yFrom, animatedData->yTo);
+            qreal newRotation = GroupTweenAnimator::mapValue(elapsedRatio, ZERO_VAL, ONE_VAL,
+                animatedData->rotationFrom, animatedData->rotationTo);
+
+            sceneObject->setX(newX);
+            sceneObject->setY(newY);
+            sceneObject->setRotation(newRotation);
+
+        } else {
+            // time is up. Got to final position.
+            sceneObject->setX(animatedData->xTo);
+            sceneObject->setY(animatedData->yTo);
+            sceneObject->setRotation(animatedData->rotationTo);
+            qDebug() << "This animation is done. You should delete this GroupTweenAnimator";
+        }
     }
-
     return hasSomeTimeLeft;
+}
+
+qreal GroupTweenAnimator::getElapsedRatio(const qint64& currentTime) const {
+    if (this->isDone(currentTime)) {
+        return 1.0;
+    } else {
+        //qreal elapsed = this->getElapsed(currentTime);
+        //qreal totalDuration = this->_duration;
+        return GroupTweenAnimator::mapValue(
+                    static_cast<qreal>(currentTime),
+                    static_cast<qreal>(this->_startTime),
+                    static_cast<qreal>(this->_endTime),
+                    ZERO_VAL, ONE_VAL);
+    }
+}
+
+qint64 GroupTweenAnimator::getElapsed(const qint64& currentTime) const {
+    qint64 ret = currentTime - this->_startTime;
+    return ret;
 }
 
 void GroupTweenAnimator::start(const qint64& currentTime) {
@@ -38,9 +108,14 @@ void GroupTweenAnimator::start(const qint64& currentTime) {
     this->_endTime = currentTime + this->_duration;
 }
 
+qint64 GroupTweenAnimator::getTimeLeft(const qint64& currentTime) const {
+    qint64 ret = this->_endTime - currentTime;
+    return ret;
+}
+
 bool GroupTweenAnimator::isDone(const qint64& currentTime) const {
     qint64 timeLeft = this->_endTime - currentTime;
-    bool ret = timeLeft < 0;
+    bool ret = timeLeft <= 0;
     return ret;
 }
 
--- mpop_dataviz/grouptweenanimator.h
+++ mpop_dataviz/grouptweenanimator.h
@@ -4,15 +4,15 @@
 #include "prisonerline.h"
 #include "sceneobject.h"
 #include <QVector>
+#include <QSharedPointer>
 
 /**
  * @brief Animates a group of scene objects.
  */
 class GroupTweenAnimator
 {
-public:
-
-    typedef struct {
+private:
+    struct AnimatedSceneObject {
         SceneObject* sceneObject;
         qreal xFrom;
         qreal xTo;
@@ -20,20 +20,26 @@ public:
         qreal yTo;
         qreal rotationFrom;
         qreal rotationTo;
-    } AnimatedObject;
+        // TODO add z and scale
+    };
 
+    typedef QSharedPointer<AnimatedSceneObject> AnimatedSceneObjectPtr;
+
+public:
     GroupTweenAnimator();
     virtual ~GroupTweenAnimator();
     /**
      * @brief Adds scene objects.
      * @param sceneObjects Scene objects to animate.
      */
-    void addSceneObjects(const QVector<SceneObject*>& sceneObjects);
+    //void addSceneObjects(const QVector<SceneObject*>& sceneObjects);
+
+    void addSceneObjectToAnimate(SceneObject* sceneObject, qreal targetX, qreal targetY, qreal targetRotation);
     /**
      * @brief Move the objects to the position they should be at now.
      * @return True if there is still some time left to the animation.
      */
-    bool updateSceneObjects(const qint64& currentTime);
+    bool updateSceneObjectsPosition(const qint64& currentTime);
 
     void start(const qint64& currentTime);
 
@@ -59,11 +65,16 @@ public:
      */
     static qreal mapValue(qreal inValue, qreal inFrom, qreal inTo, qreal outFrom, qreal outTo);
 
-protected:
-    QVector<SceneObject*> _sceneObjects;
+private:
+    // QVector<SceneObject*> _sceneObjects;
+    QVector<AnimatedSceneObjectPtr> _animatedSceneObjects;
 private:
     qint64 _duration;
     qint64 _startTime;
     qint64 _endTime;
     QEasingCurve::Type _easingType;
+
+    qreal getElapsedRatio(const qint64& currentTime) const;
+    qint64 getTimeLeft(const qint64& currentTime) const;
+    qint64 getElapsed(const qint64& currentTime) const;
 };
