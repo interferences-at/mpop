2d0260f97916ed441333418cae013be5f0c71aa6
baydam <baydamd@gmail.com>
2020-05-02 19:08:01 +0000

    Improve powershell script according to aalex recommendation

--- scripts/window-deploy-qt-app.ps1
+++ scripts/window-deploy-qt-app.ps1
@@ -10,9 +10,9 @@
   required to run the application from specific folder
 
   Requirements:
-  - Windows PowerShell 3.0 or higher.
+  - Windows PowerShell 5.0 or higher.
 
- .PARAMETER QtStaticDir
+ .PARAMETER QtDir
 
   Root directory where Qt is installed.
   By default, use C:\Qt.
@@ -21,7 +21,8 @@
 [CmdletBinding()]
 param (
   # Command line parameters
-  $QtDir = ""
+  $QtDir = "C:\Qt", # Qt default installation directory path
+  $ExeFile = "" # Path of the executable file to deploy 
 )
 
 function Main () {
@@ -29,47 +30,44 @@ function Main () {
   Add-Type -AssemblyName System.Windows.Forms | Out-Null
 
   # Get Qt root directory, if not specified on the command line
-  if (-not $QtDir) {
+  if (-not $QtDir -or !(Test-Path -PathType Container $QtDir)) {
     # Show message in order to get the user ready for take an action
     ShowMessage "A window will open very soon asking you to set the directory where Qt is installed!"
     $QtDir = [string](GetQtDirectory) # Open up folder dialog
-    # Search all instances of windeployqt.exe from $QtDir installation folder.
-    $exeFolders = @(Get-ChildItem -Path $QtDir -Filter windeployqt.exe -Recurse | ForEach-Object FullName | Sort-Object)
-
-    if ($exeFolders -eq 0) {
-      ExitScript "WinDeployQt command not found, You may not selected the right Qt directory"
-    }
-    # Get the directory path of the command
-    $windeployqtDir = Split-Path -Path $exeFolders[0]
-
-    Write-Host "Qt directory is: $QtDir"
-    # Setting up environment for Qt usage
-    SetQtEnvironment $windeployqtDir
   }
+  # Search all instances of windeployqt.exe from $QtDir installation folder.
+  $windeployqtPath = @(Get-ChildItem -Path $QtDir -Filter windeployqt.exe -Recurse | ForEach-Object FullName | Sort-Object)
+
+  if ($windeployqtPath -eq 0) {
+    ExitScript "WinDeployQt command not found, You may not selected the right Qt directory"
+  }
+
+  # Search all instances of gcc.exe from $QtDir installation folder.
+  $gccPath = @(Get-ChildItem -Path $QtDir -Filter gcc.exe -Recurse | ForEach-Object FullName | Sort-Object)
+
+  if ($gccPath -eq 0) {
+    ExitScript "GCC command not found, You may not selected the right Qt directory"
+  }
+
+  Write-Host "`nQt directory is: $QtDir"
+  Write-Host "Setting up environment for Qt usage..."
+  # Setting up environment for Qt usage
+  SetQtEnvironment @(Split-Path -Path $windeployqtPath[0])
+  SetQtEnvironment @(Split-Path -Path $gccPath[0])
   
-  
-  # Show message in order to get the user ready for take an action
-  ShowMessage "A window will open very soon asking you to load the executable file!"
-
-  # Open file dialog and asking for executable file
-  $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
-  # Make sure the the user didn't cancel the operation
-  if ($fileDialog.ShowDialog() -ne "Cancel") {
-    # Get the parent directory of the executable file
-    $exeFolder = Split-Path -Path $fileDialog.FileName
-    # Run windeployqt to get the work done 
-    windeployqt $exeFolder
-
-    ExitScript "Everything is setted up"
-  }
-  else {
-    ExitScript "You need to select the executable file that missing DLLs"
+  if (-not $ExeFile -or !(Test-Path -PathType Leaf $ExeFile)) {
+    # Show message in order to get the user ready for take an action
+    ShowMessage "A window will open very soon asking you to load the executable file!"
+    $ExeFile = [string](GetExecutableFile) # Open up file dialog
   }
 
+  # Run windeployqt to get the work done 
+  windeployqt $ExeFile
+
+  ExitScript "Everything is setted up"
 }
 
 function SetQtEnvironment ($environmentPath) {
-  Write-Host "Setting up environment for Qt usage..."
   Write-Host "Environment PATH set to $environmentPath"
   # Adding a new path to PATH
   $env:PATH = "$environmentPath;$env:PATH"
@@ -90,6 +88,22 @@ function GetQtDirectory () {
   }
 
   return $QtDir
+}
+
+function GetExecutableFile () {
+  $exeFile = ""
+  # Open file dialog and asking for executable file
+  $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
+  # Make sure the the user didn't cancel the operation
+  if ($fileDialog.ShowDialog() -ne "Cancel") {
+    # Get the parent directory of the executable file
+    $exeFile = Split-Path -Path $fileDialog.FileName
+  }
+  else {
+    ExitScript "You need to select the executable file that missing DLLs"
+  }
+
+  return $exeFile
 }
 
 function ShowMessage ([string]$message) {
