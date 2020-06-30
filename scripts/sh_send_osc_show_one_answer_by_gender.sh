531b173451cd639bf0b3c32f5bc420861fbc0e57
Alexandre Quessy <alexandre@quessy.net>
2020-06-30 08:17:52 -0400

    Fix the OSC message sent for one answer by gender
    Should fix #136

--- scripts/sh_send_osc_show_one_answer_by_gender.sh
+++ scripts/sh_send_osc_show_one_answer_by_gender.sh
@@ -5,6 +5,7 @@ MY_ROW=$((RANDOM % 3))
 MY_ANSWER=$((RANDOM % 100))
 ALL_VALUES=() # array
 GENDERS=(Hommes Femmes Autres)
+NUM_ROWS=3
 
 for i in {1..3};
 do
@@ -13,9 +14,10 @@ done
 
 set -o verbose
 
-oscsend osc.udp://localhost:31337 /dataviz/${DATAVIZ_WINDOW_INDEX}/view_answer_by_gender iisisisi \
-  $MY_ANSWER \
-  $MY_ROW \
+oscsend osc.udp://localhost:31337 /dataviz/${DATAVIZ_WINDOW_INDEX}/view_answer_by_gender iiisisisi \
+  ${NUM_ROWS} \
+  ${MY_ROW} \
+  ${MY_ANSWER} \
   ${GENDERS[0]} \
   ${ALL_VALUES[0]} \
   ${GENDERS[1]} \
