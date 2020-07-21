06785aa54ae1588ca53912be48b0ab941298dd71
Alexandre Quessy <alexandre@quessy.net>
2020-07-21 17:55:00 -0400

    Update the SQL schema

--- flyway/sql/V1__create_tables.sql
+++ flyway/sql/V1__create_tables.sql
@@ -1,26 +1,35 @@
 -- Create tables for the MPOP database
 
+-- A physical RFID tag known by our system
 CREATE TABLE IF NOT EXISTS tag
 (
-  id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-  rfid VARCHAR(256) NOT NULL UNIQUE,
-  visitor_id BIGINT UNSIGNED NULL UNIQUE,
-  last_used_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  `id` SERIAL,
+  `rfid` VARCHAR(256) NOT NULL UNIQUE,
+  `visitor_id` BIGINT UNSIGNED NULL UNIQUE,
+  `last_used_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 );
 
+-- A visitor in the exhibition
 CREATE TABLE IF NOT EXISTS visitor
 (
-  id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-  gender VARCHAR(6) DEFAULT 'other',
-  language VARCHAR(2) DEFAULT 'fr',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  `id` SERIAL,
+  `gender` ENUM('male', 'female', 'other') DEFAULT NULL,
+  `language` ENUM('fr', 'en') DEFAULT NULL,
+  `nation` ENUM('quebecer', 'canadian', 'american', 'european', 'native', 'other') NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `age` TINYINT UNSIGNED DEFAULT NULL, -- number within the range [0,255]
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 );
 
+-- An answer to a question from a visitor
 CREATE TABLE IF NOT EXISTS answer
 (
-  id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-  question_identifier VARCHAR(100),
-  value TINYINT UNSIGNED,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  `id` SERIAL,
+  `visitor_id` BIGINT UNSIGNED NOT NULL,
+  `question_identifier` VARCHAR(100) DEFAULT NULL,
+  `answer_value` TINYINT UNSIGNED NOT NULL, -- number within the range [0,100]
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 );
 
