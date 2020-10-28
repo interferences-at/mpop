3a3e714722dcb5fe03b8d10d32f66a7656d25773
Alexandre Quessy <alexandre@quessy.net>
2020-10-28 15:27:11 -0400

    Add sh_install_ubuntu.sh

---
+++ scripts/sh_install_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Install the necessary libraries and setup the groups on Ubuntu
+#
+
+apt install git terminator screen qt5-default openssh-server docker.io aptitude vim libqt5sql5-mysql qt5-default libqt5opengl5-dev libqt5serialport5-dev libqt5websockets5-dev libqt5quickcontrols2-5 liblo-tools qtdeclarative5-dev mysql-client clang-8 libqt5sql5-mysql build-essential qttools5-dev-tools qtcreator qtdeclarative5-dev qml-module-qttest qml-module-qtquick-localstorage qml-module-qtquick-extras qml-module-qtquick-dialogs qml-module-qtquick-controls2 qml-module-qt-websockets qml-module-qt-labs-settings qml-module-qt-labs-platform docker-compose terminator
+
+usermod -aG dialout ${USER}
+usermod -aG docker ${USER}
