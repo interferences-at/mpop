5f5fd40c4b3e01cbebc223165650ceadf9704593
Alexandre Quessy <alexandre@quessy.net>
2020-10-29 17:31:48 -0400

    Add startup scripts for Windows

---
+++ scripts/dataviz_run_b.ps1
@@ -0,0 +1,3 @@
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 ; 
+
