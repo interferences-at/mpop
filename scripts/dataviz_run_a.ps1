55df8f3c06ff03d504c220c0204a672823f59b4e
Alexandre Quessy <alexandre@quessy.net>
2020-10-29 17:31:48 -0400

    Add startup scripts for Windows

---
+++ scripts/dataviz_run_a.ps1
@@ -0,0 +1,3 @@
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 --align-right ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 --align-right ; 
+
---
+++ scripts/dataviz_startup_a.cmd
@@ -0,0 +1,2 @@
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\Users\utilisateur\src\mpop\scripts\dataviz_run_a.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
