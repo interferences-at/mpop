44daa87040755d6c04c430b8fd0ac9cd6a7812e7
Alexandre Quessy <alexandre@quessy.net>
2020-10-29 17:58:14 -0400

    Add the --screen option to powershell scripts

--- scripts/dataviz_run_a.ps1
+++ scripts/dataviz_run_a.ps1
@@ -1,3 +1,3 @@
-powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 --align-right ; 
-powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 --align-right ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 --align-right --screen 1 ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 --align-right --screen 2 ; 
 
--- scripts/dataviz_run_b.ps1
+++ scripts/dataviz_run_b.ps1
@@ -1,3 +1,3 @@
-powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 ; 
-powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31337 --screen 1 ; 
+powershell '& C:\Users\utilisateur\src\DataViz_1\mpop_dataviz.exe' --x-position 0 --y-position -30 --port 31338 --screen 2 ; 
 
