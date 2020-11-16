0ef458dbb1024f3fb30e0f6bfdc08a51d8127823
Alexandre Quessy <alexandre@quessy.net>
2020-11-16 15:54:46 -0500

    Run mysql outside of docker

--- scripts/sh_start_service_without_docker.sh
+++ scripts/sh_start_service_without_docker.sh
@@ -3,7 +3,10 @@
 # Starts the MPOP Service
 #
 
-CONFIG_MYSQL_PORT=13306 \
-    CONFIG_MYSQL_HOST=127.0.0.1 \
+CONFIG_MYSQL_PORT=3306 \
+    CONFIG_MYSQL_HOST=localhost \
+    CONFIG_MYSQL_USER=root \
+    CONFIG_MYSQL_DATABASE=mpop_database \
+    CONFIG_MYSQL_PASSWORD=secret \
     ~/src/mpop/mpop_service/mpop_service
 
